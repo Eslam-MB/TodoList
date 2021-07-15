e95fb57bf727c43ea55a6562df9882f4cfb0fbcf
Eslam-MB <islam10mb@gmail.com>
2021-07-15 16:46:35 +0200

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TodosComponent } from "./components/todos/todos.component";
+import { FormsModule } from "@angular/forms";
+@NgModule({
+  declarations: [AppComponent, TodosComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from "@angular/core";
+import { Todo } from "./../../models/Todo";
+@Component({
+  selector: "app-todos",
+  templateUrl: "./todos.component.html",
+  styleUrls: ["./todos.component.scss"],
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[] | undefined;
+  inputTodo: string = "";
+  constructor() {}
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content: "first todo",
+        completed: false,
+      },
+      {
+        content: "second todo",
+        completed: false,
+      },
+    ];
+  }
+  toggleDone(id: number) {
+    this.todos?.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+
+      return v;
+    });
+  }
+  deleteTodo(id: number) {
+    this.todos = this.todos?.filter((v, i) => i !== id);
+  }
+  addTodo() {
+    this.todos?.push({
+      content: this.inputTodo,
+      completed: false,
+    });
+    this.inputTodo = "";
+  }
+}
